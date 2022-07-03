0c72446ff55330a4f111ba33ed077cc89c93ff1c
Nada Maisara <101016405+nadamaisara@users.noreply.github.com>
2022-07-03 05:09:50 +0200

    Add files via upload

---
+++ Start_Window.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N_Puzzle_Game
+{
+    public partial class Start_Window : Form
+    {
+        public static Start_Window __main__;
+        bool Eight = false, Fifteen = false;
+        public Start_Window()
+        {
+            InitializeComponent();
+            __main__ = this;
+        }
+
+        private void Start_Window_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbx_size.SelectedItem.ToString())
+            {
+                case "3 * 3":
+                    comboBox2.Items.Clear();
+                    List<string> lst = new List<string>() { "BFS" };
+                    foreach (string s in lst) comboBox2.Items.Add(s);
+                    Eight = true; Fifteen = false;
+                    break;
+       
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Eight_Puzzle _8 = new Eight_Puzzle();
+
+            if (Eight)
+            {
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "BFS":
+                        _8.b_bfs = true;
+                        break;
+                }
+                _8.Show();
+                __main__.Hide();
+            }
+        }
+
+    }
+}
---
+++ UserControl_Need.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N_Puzzle_Game
+{
+    public class UserControl_Need : UserControl
+    {
+        public Timer t;
+        public List<string> rand_states = new List<string>();
+        public int btn_length = 0, w_length = 0, N = 0;
+
+        public int x { set; get; }
+        public int y { set; get; }
+
+        public void load()
+        {
+            string path = @"Files\input4_4.in";
+            var lines = File.ReadLines(path);
+            foreach (var line in lines)
+                rand_states.Add(line.ToString());
+        }
+
+        public string state { set; get; }
+
+        public string suffle(string p)
+        {
+            Random rand = new Random();
+            char[] state = p.ToCharArray();
+            for (int i = 0; i < p.Length - 1; i++)
+            {
+                int x = rand.Next(i + 1, p.Length);
+                char c = state[i];
+                state[i] = state[x];
+                state[x] = c;
+            }
+            return new string(state);
+        }
+
+        public bool is_solvable(string state)
+        {
+            int ans = 0;
+            for (int i = 0; i < state.Length - 1; i++)
+                for (int j = i + 1; j < state.Length; j++)
+                    if (state[j] > state[i] && state[i] != 48)
+                        ans += 1;
+            return (ans % 2 == 0);
+        }
+
+        public string get_state(int size)
+        {
+            string ans = "";
+            for (char i = '0'; i < '0' + size; i++)
+                ans += i;
+            return suffle(ans);
+        }
+
+        public void start()
+        {
+            t.Start();
+        }
+
+        public string random_state()
+        {
+            Random rand = new Random();
+            return rand_states[rand.Next(0, rand_states.Count)];
+        }
+    }
+}
